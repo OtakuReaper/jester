8f2e92bd0f4fa5cfc569af1b14abf42ed1f54edd
Michael <junior.developer@belizefsc.org.bz>
2025-11-21 15:42:49 -0600

    SAVE: started working on the frontend

--- jester_app/src/App.tsx
+++ jester_app/src/App.tsx
@@ -1,6 +1,8 @@
 import axios from "axios"
 import './App.css'
 import { useEffect, useState } from "react"
+import {Row, Table, type TableProps} from "antd"
+import Column from "antd/es/table/Column";
 
 function App() {
 
@@ -21,13 +23,47 @@ function App() {
     SetPing(data.data.message);
   }
 
+  interface budgetType {
+    id: Number,
+    name: String,
+    allocated: Number,
+    spent: Number,
+    color: String,
+  } 
+
+  const budgetColumns: TableProps<budgetType>['columns'] = [
+    {
+      title: 'Budget',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text) => <p>{text}</p>,
+    }
+  ]
+
+  const budgets: budgetType[] = [
+    { id: 1, name: "Groceries", allocated: 86.60, spent: 86.60, color: "#d9d2e9"},
+    { id: 2, name: "Disgressionary Food", allocated: 45, spent: 34.50, color: "#d9d2e9"},
+    { id: 3, name: "Gym", allocated: 45, spent: 0, color: "#d9d2e9"},
+    { id: 4, name: "Car Gas", allocated: 100, spent: 100, color: "#d9d2e9"}
+  ]
+
   useEffect(() => {
-
+    if (budgets.length > 0) {
+      SetPing("Budgets Loaded")
+    }
   }, [SetPing, ping])
-
 
   return (
     <>  
+    <div>
+        <p>{budgets[1].name}</p>
+    </div>
+
+    <Table<budgetType> dataSource={budgets} columns={budgetColumns} rowKey="id" pagination={false}>
+
+    </Table>
+      
+
       <button onClick={() => pingHandler()}>
         {ping}
       </button>
