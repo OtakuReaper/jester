db405a86cbd2fbc221f4db84e59c6260f645020e
Michael <junior.developer@belizefsc.org.bz>
2026-01-07 16:57:50 -0600

    seeded the database

--- db/m1/initial_up.sql
+++ db/m1/initial_up.sql
@@ -1,14 +1,14 @@
 create table if not exists user_statuses (
     id          uuid primary key default gen_random_uuid() not null,
     name        varchar(16) not null,
-    description text,
+    description text
 );
 
 create table if not exists update_types(
     id         uuid primary key default gen_random_uuid() not null,
     name        varchar(16) not null,
-    description text,
-)
+    description text
+);
 
 create table if not exists users (
     id              uuid primary key default gen_random_uuid() not null,
@@ -21,26 +21,26 @@ create table if not exists users (
     updated_at      timestamptz default now() not null,
     created_by      uuid references users(id) not null,
     updated_by     uuid references users(id) not null,
-    update_type_id uuid references update_types(id) not null,
+    update_type_id uuid references update_types(id) not null
 );
 
 create table if not exists budget_types (
     id              uuid primary key default gen_random_uuid() not null,
     name            varchar(16) not null,
-    description     text,
+    description     text
 );
 
 create table if not exists periods(
     id             uuid primary key default gen_random_uuid() not null,
     user_id        uuid references users(id) not null,
     start_date     timestamptz not null,
-    end_date       timestamptz not null,
+    end_date       timestamptz not null
 );
 
 create table if not exists entry_types (
     id             uuid primary key default gen_random_uuid() not null,
     name           varchar(16) not null,
-    description    text,
+    description    text
 );
 
 create table if not exists entries (
@@ -50,7 +50,7 @@ create table if not exists entries (
     period_id      uuid references periods(id) not null,
     description    text not null,
     date           timestamptz not null,
-    amount         decimal(12,2) not null,
+    amount         decimal(12,2) not null
 );
 
 create table if not exists budgets(
@@ -64,5 +64,29 @@ create table if not exists budgets(
     updated_at     timestamptz default now() not null,
     created_by     uuid references users(id) not null,
     updated_by     uuid references users(id) not null,
-    update_type_id uuid references update_types(id) not null,
+    update_type_id uuid references update_types(id) not null
 );
+
+
+-- Seeding initial data
+
+insert into user_statuses (name, description) values
+('active', 'Active user'),
+('inactive', 'Inactive user');
+
+insert into update_types (name, description) values
+('update', 'Updates');
+
+insert into budget_types (name, description) values
+('normal', 'Normal budget'),
+('fixed', 'Fixed budget'),
+('savings', 'Savings budget'),
+('income', 'Income budget'),
+('debt', 'Debt budget');
+
+insert into entry_types (name, description) values
+('expense', 'Expense entry'),
+('income', 'Income entry');
+
+-- 
+
